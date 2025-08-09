4d46aaf196fba886d8bac2ccbdcee0f77b4fb36a
Shivasundar12 <shiva6sundar@gmail.com>
2025-08-09 11:57:19 +0530

    python if else

---
+++ IF-ELSE.py
@@ -0,0 +1,12 @@
+# what is if else statement in python?
+# An if-else statement in Python is a control flow statement that allows you to execute certain blocks of code based on whether a condition is true or false. It helps in making decisions in your code.
+
+#exmaple of if else statement 
+
+age=int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+# In this example, the program checks if the entered age is 18 or older. If it is, it prints "You are an adult." Otherwise, it prints "You are a minor."
