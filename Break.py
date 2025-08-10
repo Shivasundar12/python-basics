7c62d5f4587de7c33eda572e48475d7cc3b2d9c9
Shivasundar12 <shiva6sundar@gmail.com>
2025-08-10 20:35:16 +0530

    break-statement

---
+++ Break.py
@@ -0,0 +1,4 @@
+# 1. break → Stop the loop completely
+#Ends the loop right away, even if there are more items left to process.
+
+#Used when you found what you were looking for and don’t need to continue.
