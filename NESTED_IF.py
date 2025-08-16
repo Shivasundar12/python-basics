226679f138e139c5ab4c8d05690160d22c80dd4b
Shivasundar12 <shiva6sundar@gmail.com>
2025-08-16 19:45:51 +0530

    ..

--- NESTED_IF.py
+++ NESTED_IF.py
@@ -1,24 +1,35 @@
-# Program to demonstrate nested if statements
+# # Program to demonstrate nested if statements
 
-num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
 
-if num > 0:
-    print("The number is positive.")
-    if num % 2 == 0:
-        print("It is also even.")
-    else:
-        print("It is also odd.")
+# if num > 0:
+#     print("The number is positive.")
+#     if num % 2 == 0:
+#         print("It is also even.")
+#     else:
+#         print("It is also odd.")
+# else:
+#     print("The number is not positive.")
+
+# age = 20
+# citizen = False
+
+# if age >= 18:                 # Stage 1: Age check
+#     print("You are an adult.") # Action 1
+#     if citizen:               # Stage 2: Citizenship check
+#         print("You can vote!") # Action 2
+#     else:
+#         print("But you are not a citizen, so you cannot vote.") # Different action
+# else:
+#     print("You are not an adult.") # Action if Stage 1 fails
+
+
+# Odd or Even
+
+num1 = int(input(("enter a number: ")))
+
+if  num1 % 2 == 0:
+    print("The number is even.")
 else:
-    print("The number is not positive.")
+    print("The number is odd.")
 
-age = 20
-citizen = False
-
-if age >= 18:                 # Stage 1: Age check
-    print("You are an adult.") # Action 1
-    if citizen:               # Stage 2: Citizenship check
-        print("You can vote!") # Action 2
-    else:
-        print("But you are not a citizen, so you cannot vote.") # Different action
-else:
-    print("You are not an adult.") # Action if Stage 1 fails
---
+++ conditional.py
@@ -0,0 +1,15 @@
+# conditional expression = if <condition> then <expression1> else <expression2> A one-line shortcut for if else statement this is also called as conditional operator 
+
+# program to demonstrate conditional operator:  x if <condition> else y "this is the formul!"
+
+
+# age = int(input("Enter your age: "))
+temperature = int(input("Enter the temperature: ")) 
+
+# print( "good to learn driving" if age >= 21 else "you are not eligible to learn driving") 
+
+# result = "good to learn driving" if age >= 21 else "you are not eligible to learn driving"
+# print(result)
+
+MO = "It's a hot day" if temperature > 30 else "It's a cold day"
+print(MO)
